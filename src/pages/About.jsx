d144d6040257f00f97d1fad03cdc95e199f8c6b4
Thiago Scholl <tcscholl@inf.ufpel.edu.br>
2023-05-28 23:50:02 -0300

    Adicionando Router, componentes Navbar e Footer e paginas About, Home e NotFound

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-5xl mb-4">Github Finder</h1>
+            <p className='mb-4 text-2xl font-light'>
+            A React app to search GitHub profiles and see profile details.
+            </p>
+            <p className='text-lg text-gray-400'>
+                Version <span className="text-gray-600">1.0.0</span>
+            </p>
+            <p className='text-lg text-gray-400'>
+                Layout By:
+            <a className='text-white' href=''>
+                
+            </a>
+      </p>
+        </div>
+    )
+}
+
+export default About
